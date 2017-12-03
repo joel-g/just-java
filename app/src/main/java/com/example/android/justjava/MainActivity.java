13e202c6b4aa5e69e09910805d3d2c70c49742a0
Joel Guerra <joelaguerra1@gmail.com>
2017-12-02 19:30:16 -0800

    improved price display

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        String priceMessage = "Free!";
+        String priceMessage = "Total: $" + (quantity * 5);
         displayMessage(priceMessage);
     }
 
